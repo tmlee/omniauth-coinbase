34056d41371664491009f542de9e66555ca7a58b
TM Lee <tm89lee@gmail.com>
2014-03-10 16:27:45 +0800

    Add additional & scope params

--- lib/omniauth/strategies/coinbase.rb
+++ lib/omniauth/strategies/coinbase.rb
@@ -24,6 +24,16 @@ module OmniAuth
         { :raw_info => raw_info }
       end
 
+      def authorize_params
+        super.tap do |params|
+          %w[scope meta[name] meta[send_limit_amount] meta[send_limit_currency] meta[send_limit_period]].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+        end
+      end
+
       def raw_info
         @raw_info ||= MultiJson.load(access_token.get('/api/v1/users').body)['users'][0]['user']
       rescue ::Errno::ETIMEDOUT
